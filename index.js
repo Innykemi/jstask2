52e42597606dce3cb571459b9db65dab6031db9d
Iniobong Oluwakemi <oluwakemiraimi.nig@gmail.com>
2020-04-06 23:32:56 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+let data = [
+  {
+    Principal: 2500,
+    time: 1.8
+  },
+  {
+    Principal: 1000,
+    time: 5
+  },
+  {
+    Principal: 3000,
+    time: 1
+  },
+  {
+    Principal: 2000,
+    time: 3
+  }
+];
+
+function interestCalculator(array) {
+  let interestData = [];
+
+  for (let i in array) {
+    if (array[i].Principal >= 2500 && array[i].time > 1 && array[i].time <= 3) {
+      array[i].rate = 3;
+    } else if (array[i].Principal >= 2500 && array[i].time >= 3) {
+      array[i].rate = 4;
+    } else if (array[i].Principal < 2500 || array[i].time <= 1) {
+      array[i].rate = 2;
+    } else {
+      array[i].rate = 1;
+    }
+    
+    interest = (array[i].Principal * array[i].rate * array[i].time) / 100;
+    array[i].interest = interest;
+    
+    interestData.push({
+        principal: array[i].Principal,
+        rate: array[i].rate,
+        time: array[i].time,
+        interest: array[i].interest
+    });
+  }
+  console.log(interestData);
+  return;
+}
+interestCalculator(data);
